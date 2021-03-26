82fe216db518a01b0d5e7f24064e9520e073d91b
Evan Peterschick <expeter8@gmail.com>
2021-03-25 22:40:18 -0500

    added fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! Javascript is running!");
+function fight() {
+    window.alert("the fight has begun!");
+}
+// fight();
+var playerName=window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This Logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our Robot's name is" + playerName);
